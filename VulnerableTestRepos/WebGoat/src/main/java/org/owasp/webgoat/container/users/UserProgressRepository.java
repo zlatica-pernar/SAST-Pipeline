98bcc9b6e53fc3b42797cd2145291e9013132ab0
zlatica-pernar <129206741+zlatica-pernar@users.noreply.github.com>
2024-11-09 12:49:12 +0100

    delete repos

--- VulnerableTestRepos/WebGoat/src/main/java/org/owasp/webgoat/container/users/UserProgressRepository.java
+++
@@ -1,9 +0,0 @@
-package org.owasp.webgoat.container.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserProgressRepository extends JpaRepository<UserProgress, String> {
-
-  // TODO: make optional
-  UserProgress findByUser(String user);
-}
--- VulnerableTestRepos/WebGoat/src/main/java/org/owasp/webgoat/server/ParentConfig.java
+++
@@ -1,6 +0,0 @@
-package org.owasp.webgoat.server;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ParentConfig {}
