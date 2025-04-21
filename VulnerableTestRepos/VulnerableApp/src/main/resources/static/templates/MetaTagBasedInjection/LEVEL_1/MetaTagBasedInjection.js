c60fd4a48497adc2f8b18c0eee84dd20b2a55e6a
zlatica-pernar <129206741+zlatica-pernar@users.noreply.github.com>
2025-04-21 19:57:22 +0200

    removed vulnerableApp

--- VulnerableTestRepos/VulnerableApp/src/main/resources/static/templates/MetaTagBasedInjection/LEVEL_1/MetaTagBasedInjection.js
+++
@@ -1,7 +0,0 @@
-function updatePlaceholderDiv() {
-  let placeholderAnchorElement = document.getElementById("placeholder");
-  placeholderAnchorElement.href = getUrlForVulnerabilityLevel() + "?returnTo=/";
-  placeholderAnchorElement.innerText = "Click here";
-}
-
-updatePlaceholderDiv();
